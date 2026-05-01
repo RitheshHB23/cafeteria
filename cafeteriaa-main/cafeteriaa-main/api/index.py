cd4f2a6310e89c2673395a0fbfe8132c92e746a4
RitheshHB23 <rithuu20077@gmail.com>
2026-05-01 10:35:25 +0530

    Delete cafeteriaa-main/cafeteriaa-main directory

--- cafeteriaa-main/cafeteriaa-main/api/index.py
+++
@@ -1,7 +0,0 @@
-import sys
-from pathlib import Path
-
-# Add the parent directory to sys.path so that 'server' can be imported
-sys.path.append(str(Path(__file__).parent.parent))
-
-from server import app
